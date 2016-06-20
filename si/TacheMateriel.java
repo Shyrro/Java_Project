b8afe1c2e7b75780b95739e964a2e7ded7e6d636
mccyp <cyprien.lavigne@hotmail.com>
2016-06-21 00:47:29 +0200

    Update TacheMateriel.java

--- si/TacheMateriel.java
+++ si/TacheMateriel.java
@@ -1,6 +1,13 @@
 package si;
 
+import bibagenda.*;
+
 
 public class TacheMateriel extends TacheInformatique {
 
+	public TacheMateriel(String nature, int duree){
+		super(nature,duree);
+	}
+
 }
+
