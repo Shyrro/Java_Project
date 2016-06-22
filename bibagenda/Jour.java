649b3ec9708cd0ad839d58a6f4b3183a67b5e1a6
Shyrro <zsahmane@gmail.com>
2016-06-22 17:22:01 +0200

    Update Jour.java

--- bibagenda/Jour.java
+++ bibagenda/Jour.java
@@ -2,6 +2,22 @@ package bibagenda;
 
 public enum Jour {
 
-	Lundi, Mardi, Mercredi, Jeudi, Vendredi
-	
+	Lundi("Lundi"), Mardi("Mardi"), Mercredi("Mercredi"), Jeudi("Jeudi"), Vendredi("Vendredi");
+
+	private final String name;
+
+	private Jour(String s){
+		this.name = s;
+	}
+
+	public String toString(){
+		return this.name;
+	}
+
+	public boolean equals(Jour j){
+
+		return this.name.equals(j.name);
+
+	}
+
 }
