b65fdda1d95e2863433c34171b30cb072424eba2
Shyrro <zsahmane@gmail.com>
2016-06-22 17:25:20 +0200

    Update TacheLogiciel.java

--- si/TacheLogiciel.java
+++ si/TacheLogiciel.java
@@ -4,9 +4,20 @@ import bibagenda.*;
 
 
 public class TacheLogiciel extends TacheInformatique {
+	
+	private Logiciel logiciel;
 
-	public TacheLogiciel(String nature, int duree){
+	public TacheLogiciel(String nature, int duree,Logiciel logiciel){
 		super(nature,duree);
+		this.logiciel = logiciel;
+	}
+
+	public Logiciel getLogiciel(){
+		return this.logiciel;
+	}
+
+	public String toString(){
+		return "TachNum : "+this.idTache + " ,NatureIntervention: "+this.natureIntervention+", Logiciel :"+this.logiciel;
 	}
 
 }
