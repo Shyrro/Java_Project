3bd1a5fa1c1366b7f7b22f44291675cacbbcb13f
Shyrro <zsahmane@gmail.com>
2016-06-22 17:24:37 +0200

    Update ServiceInfo.java

--- si/ServiceInfo.java
+++ si/ServiceInfo.java
@@ -1,56 +1,45 @@
 package si;
-import java.util.ArrayList;
-import java.util.Map;
-
+import java.util.*;
 import bibagenda.*;
 
 
 public class ServiceInfo extends Service {
 
 		//Attributs
-		ArrayList<Materiel> materiels;
-		ArrayList<Logiciel> logiciels;
+		private ArrayList<Materiel> materiels;
+		private ArrayList<Logiciel> logiciels;
 		private ArrayList<TacheMateriel> tachesEnAttente;
 
 		//Constructeur
 		public ServiceInfo(String nom){
 			super(nom);
+			tachesEnAttente = new ArrayList<TacheMateriel>();			
 		}
-
-
 
 		public ServiceInfo(String nom, Map<String, Employe> employes) {
 			super(nom, employes);
 			tachesEnAttente = new ArrayList<TacheMateriel>();
 		}
-		
 
 		
-		
 		//methodes
+		public void setTachesEnAttente(TacheMateriel t){
+			this.tachesEnAttente.add(t);
+		}
+
 		public ArrayList<TacheMateriel> getTachesEnAttente() {
 			return tachesEnAttente;
 		}
-
-		public void setTachesEnAttente(ArrayList<TacheMateriel> tachesEnAttente) {
-			this.tachesEnAttente = tachesEnAttente;
-		}
-
-		//Methodes			
+			
 		public void affecterTache(Tache t) {
 			try{
-				Employe e = new Employe("toto");
-				e = this.getEmployeDispo(t);
+				Employe e = this.getEmployeDispo(t);
 
 				if(e!=null){
-					if(t instanceof TacheLogiciel)
-						for(Creneau c : e.getCreneauxLibresEmploye(t.getNbCreneaux()))
+						for(Creneau c : e.getCreneauxLibresEmploye(t.getNbCreneaux())) 
 							c.setTache(t);
-					else if(t instanceof TacheMateriel){
-						for(Creneau c : e.getCreneauxLibresEmploye(t.getNbCreneaux()))
-							c.setTache(t);
-					}
-				}else{
+				}
+				else{
 					if(t instanceof TacheMateriel)
 						this.tachesEnAttente.add((TacheMateriel)t);
 				}
@@ -59,5 +48,35 @@ public class ServiceInfo extends Service {
 			}catch(ClassCastException e){
 			}catch(NullPointerException e){
 			}
-		}		
+		}
+
+		public void supprimerTache(Tache t) {
+		
+		
+			//Recuperation de tous les creneaux
+			List<Creneau> creneaux = this.getAllEdt();
+		
+			//On met a null tous les creneaux occupes par la tache t
+			for(Creneau c : creneaux){
+				if(c.getTache()!=null)
+				{
+					if(c.getTache().equals(t))
+						c.setTache(null);
+				}
+			}
+			// on va rechercher si on peut remplacer cette tache par une des taches en attente
+			Iterator<TacheMateriel> iterator = this.tachesEnAttente.iterator();
+			
+			boolean remplacee=false;
+			while( iterator.hasNext() && remplacee==false) {
+				TacheMateriel tm=iterator.next();
+				if(this.getEmployeDispo(tm)!=null) {
+					affecterTache(tm);
+					remplacee=true;
+					this.tachesEnAttente.remove(tm);
+				}
+			}
+		
+		}
+				
 }
