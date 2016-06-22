3aa6b9f9fe9e5885223f2a4000d2cf787208541e
Shyrro <zsahmane@gmail.com>
2016-06-22 17:25:34 +0200

    Update TacheMateriel.java

--- si/TacheMateriel.java
+++ si/TacheMateriel.java
@@ -5,9 +5,18 @@ import bibagenda.*;
 
 public class TacheMateriel extends TacheInformatique {
 
-	public TacheMateriel(String nature, int duree){
+	private Materiel materiel;
+
+	public TacheMateriel(String nature, int duree,Materiel materiel){
 		super(nature,duree);
+		this.materiel = materiel;
 	}
 
-}
+	public Materiel getMateriel(){
+		return this.materiel;
+	}
 
+	public String toString(){
+		return "TacheNum :"+this.idTache + " ,NatureIntervention: "+this.natureIntervention+", Materiel :"+this.materiel;
+	}
+}
